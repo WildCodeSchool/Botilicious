b17cffb8a85df7638af2d1c978e7623e9cd90e1a
FabWild <fab@dfweb.fr>
2017-11-22 22:50:36 +0100

    meteo version 0.1 ok

--- routes/main.js
+++ routes/main.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const mysql = require('mysql');
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 const config = require('../config.js');
 const connection = mysql.createConnection(config);
@@ -12,60 +13,104 @@ connection.connect();
 
 /* GET Affichage de la page de visualisation */
 router.get('/botilicious', function (req, res, next) {
-    res.render('configchat');
+  res.render('configchat');
 });
 
 /* GET Affichage de la page de mise à jour de mise à jour des infos personnelles */
 router.get('/miseajour', function (req, res, next) {
-    res.render('update');
+  res.render('update');
 });
 
 
 /* POST Prise en compte des informations de mise à jour
 router.post('/miseajour/:idWilder(\\d+)', function(req, res, next){
-	connection.query('UPDATE wilders SET prenom = ?, nom = ?, email = ?, motdepasse = ?, naissance = ?, adresse = ?, codepostal = ?, ville = ?, WHERE id = ?', [req.body.prenom, req.body.nom, req.body.email, req.body.motdepasse, req.body.naissance, req.body.adresse, req.body.codepostal, req.body.ville], function (error) {
-		if (error) {
-			console.log(error);
-		} else {
-			res.redirect('/confirmationmaj');
-		}
-	})
+connection.query('UPDATE wilders SET prenom = ?, nom = ?, email = ?, motdepasse = ?, naissance = ?, adresse = ?, codepostal = ?, ville = ?, WHERE id = ?', [req.body.prenom, req.body.nom, req.body.email, req.body.motdepasse, req.body.naissance, req.body.adresse, req.body.codepostal, req.body.ville], function (error) {
+if (error) {
+console.log(error);
+} else {
+res.redirect('/confirmationmaj');
+}
+})
 }); */
 
 
 /* GET Affichage de la page de confirmation de mise à jour */
 router.get('/confirmationmaj', function (req, res, next) {
-    res.render('confirmmaj');
+  res.render('confirmmaj');
 });
 
 
 /* GET Affichage de la page d'administration des données personnelles dans le formulaire de mise à jour
 router.post('/miseajour/idWilder(\\d+)', function (req, res, next) {
-	connection.query('select * from wilders where id= ?', [req.params.idWilder], function (error) {
-			if (error) {
-				console.log(error);
-			} else {
-				res.render('update'), {
-					wilders = results[0]
-				}
-			});
-	});
+connection.query('select * from wilders where id= ?', [req.params.idWilder], function (error) {
+if (error) {
+console.log(error);
+} else {
+res.render('update'), {
+wilders = results[0]
+}
+});
+});
 }); */
 
 router.get('/configchat', function (req, res, next) {
-    res.render('configchat');
+  res.render('configchat');
 });
 
 router.post('/postamessage', function(req, res, next) {
-  console.log(req.body);
-  res.send('bob');
+  console.log(req.body.message);
+  let xmlhttp = new XMLHttpRequest();
+  // console.log('bob');
+  // console.log(xmlhttp);
+  xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.status == 200 && xmlhttp.readyState == 4) {
+      let data = JSON.parse(xmlhttp.responseText);
+      console.log(data.list[0].weather[0].description);
+      res.send("Weather in "+req.body.message+": "+data.list[0].weather[0].description);
+    }
+    else if (xmlhttp.status == 400) {
+      console.log("Error 400");
+    }
+    else {
+      console.log("Statut de la réponse: %d (%s) state:", xmlhttp.status, xmlhttp.statusText, xmlhttp.readyState);
+    }
+    console.log(xmlhttp.status, xmlhttp.readyState);
+  };
+  xmlhttp.open("GET", "http://api.openweathermap.org/data/2.5/forecast?q="+req.body.message+"&APPID=7081077244653a5c7f8f9ab6496d6bd3", true);
+  xmlhttp.send();
+  // $(document).ready(function(){
+  // $.getJSON("http://api.openweathermap.org/data/2.5/forecast?q=Paris&APPID=7081077244653a5c7f8f9ab6496d6bd3",function(result){
+  // // $.getJSON("http://api.openweathermap.org/data/2.5/forecast?q="+req.body.message+"&mode=json&units=metric&cnt=10&APPID=7081077244653a5c7f8f9ab6496d6bd3",function(result){
+  //   console.log(result);
+  //   res.send("City: "+ result.city.name +" - Weather: "+ result.list[0].weather[0].description);
+  // });
+  // });
+
+
+  //api meteo
+  // $.ajax({
+  //   url: "http://api.openweathermap.org/data/2.5/weather",
+  //   jsonp: "callback",
+  //   dataType: "jsonp",
+  //   data: {
+  //     // id: "2172797",
+  //     APPID: "7081077244653a5c7f8f9ab6496d6bd3"
+  //   },
+  //   success: function(response) {
+  //     console.log(response); // server response
+  //     res.send(response.weather[0].main);
+  //   }
+  // });
+
+
+  // res.send('bob');
   // res.sendStatus(200);
 });
 
 /* GET /admin/create
 router.get('/create', function(req, res, next) {
-	// Formulaire de création d'article
-	res.render('admin-create');
+// Formulaire de création d'article
+res.render('admin-create');
 }); */
 
 module.exports = router;
