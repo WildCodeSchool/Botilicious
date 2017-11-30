84c0a5f7e67d4a65d6162152ebbf55932cba74e1
jeremy d <jeremy.david94@gmail.com>
2017-11-30 18:52:13 +0100

    creation du model MVC

---
+++ controllers/Indexes.js
@@ -0,0 +1,111 @@
+const validator = require('validator');
+const nodemailer = require("nodemailer");
+
+var Indexes = {
+    
+      
+    erreur404: function(req, res){
+        res.render('index/error');
+    },
+
+    indexGet: function (req, res, next) {    
+        console.log('render index');
+        res.render('index/index');
+    },
+      
+
+    indexPost: function (req, res, next) {
+        let error=[];
+        console.log(validator.isEmpty(req.body.prenom));
+        if(validator.isEmpty(req.body.prenom) ){
+            error[0] = "Veuillez renseigner votre prenom";
+            console.log(error.length);
+        }
+        if(validator.isEmpty(req.body.nom) ){
+            error[1] = "Veuillez renseigner votre nom";
+            console.log(error.length);
+        }
+        if(validator.isEmpty(req.body.email) ){
+            error[2] = "Veuillez renseigner votre identifiant";
+            console.log(error.length);
+        }
+        if(validator.isEmpty(req.body.motdepasse) ){
+            error[3] = "Veuillez renseigner votre mot de passe";
+            console.log(error.length);
+        }
+        if(error.length>0){
+                console.log(error);
+                res.render('index', {error: error});
+            }else{
+                res.redirect('/confirminscription');
+            }
+    },
+
+
+    emailsending: function(req, res, next) {
+        var transport = nodemailer.createTransport({
+            host: "smtp.mailtrap.io",
+            port: 2525,
+            auth: {
+              user: "bad2f38eae6508",
+              pass: "359d3004ac5407"
+            }
+          });
+        transport.sendMail({
+              from: "bob@gmail.com", // Expediteur
+              to: "supermario@gmail.com", // Destinataires
+              subject: "Cookies", // Sujet
+              text: "Hello, to confirm your account on Botilicious, click on the following url :", // plaintext body
+              html: "<b>Hello world ✔</b>" // html body
+          }, (error, response) => {
+        if(error){
+        console.log(error);
+              }else{
+        console.log("Message sent: " + response.message);
+              }
+          });
+    },
+
+    confirmationinscription: function (req, res, next) {
+            res.render('index/confirminscription');
+    },
+
+    connexionGet: function (req, res, next) {
+        res.render('index/login');
+    },
+
+    deconnexion: function (req, res, next) {
+        res.render('index/deconnexion');
+    },
+    
+    motdepasseoublie: function (req, res, next) {
+        res.render('index/oubli');
+    },
+
+    connexionPost: function (req, res, next) {
+        console.log('login en cours');
+        console.log(req.body);
+        let login = req.body.email;
+        let pass = req.body.motdepasse;
+        console.log(login, pass);
+    
+    // requête sequelize sur la table Users
+        // connection.query('SELECT * FROM users WHERE email = ? AND password = ? ;',[login, pass],function (error, results, fields) {
+        //   if (error) throw error;
+        //   if (results.length === 0) {
+        //     console.log('error login');
+        //     res.redirect('/connexion');
+        //   } else {
+        req.session.connected = true;
+    
+    // inscrire l'id de l'utilisateur dans req.session
+        // req.session.user = results[0].id;
+        console.log(req.session);
+        res.redirect('/main/');
+    },
+
+
+      
+};
+
+module.exports = Indexes; 
--- routes/main.js
+++ routes/main.js
@@ -1,19 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const request = require('request');
+
+
+var mains = require('../controllers/Mains'); // permet de faire la liaison avec le controlleur 'Mains.js'
 
 /* Projet IAforall - Botilicious Ce fichier regroupe les routes des pages accessibles post-connexion */
 
 
 /* GET Affichage de la page de visualisation */
-router.get('/', function (req, res, next) {
-  res.render('configchat');
-});
+router.get('/', mains.index);
 
 /* GET Affichage de la page de mise à jour de mise à jour des infos personnelles */
-router.get('/miseajour', function (req, res, next) {
-  res.render('update');
-});
+router.get('/miseajour', mains.miseajour);
 
 
 /* POST Prise en compte des informations de mise à jour
@@ -29,9 +27,7 @@ res.redirect('/confirmationmaj');
 
 
 /* GET Affichage de la page de confirmation de mise à jour */
-router.get('/confirmationmaj', function (req, res, next) {
-  res.render('confirmmaj');
-});
+router.get('/confirmationmaj', mains.confirmationmaj);   
 
 
 /* GET Affichage de la page d'administration des données personnelles dans le formulaire de mise à jour
@@ -47,54 +43,9 @@ wilders = results[0]
 });
 }); */
 
-router.get('/configchat', function (req, res, next) {
-  res.render('configchat');
-});
+router.get('/configchat', mains.configchat);
 
-router.post('/postamessage', function(req, res, next) {
-  console.log(req.body.message);
-  let message = req.body.message.split(' ');
-
-  let time;
-  let errorsyntaxe;
-  if (message.length == 3 && typeof(parseInt(message[2]))=== 'number' && typeof(parseInt(message[1]))=== 'number'){
-    time = 8*message[1] + Math.round(message[2]/3);
-  } else if (message.length == 2 && typeof(parseInt(message[1]))=== 'number'){
-    time = 8*message[1];
-  } else {
-    time = 0;
-    let errorsyntaxe='votre syntaxe est incorrecte';
-  }
-  if (time > 39) {
-    time = 39;
-  }
-  console.log("time:", time);
-  if (isNaN(time)){
-    errorsyntaxe='votre syntaxe est incorrecte';
-    time=0;
-  }
-
-  request("http://api.openweathermap.org/data/2.5/forecast?q="+message[0]+"&APPID=7081077244653a5c7f8f9ab6496d6bd3", function(error, response, body){
-      console.log(JSON.parse(response.body));
-
-      let data = JSON.parse(response.body);
-
-      let temp = Math.round(data.list[time].main.temp-273.15);
-
-      console.log(temp);
-
-      let responseapi = {Time: data.list[time].dt_txt+" ", City : data.city.name+" ", Country : data.city.country, Weather : data.list[time].weather[0].description+" ", Temperature : temp};
-      console.log('reponse :',responseapi);
-      // res.end();
-
-      res.json(responseapi);
-      });
-  //   }
-  // )
-  // .catch(function(err) {
-  //   console.log('Fetch Error :-S', err);
-  // });
-});
+router.post('/postamessage', mains.postamessage); 
 
 /* GET /admin/create
 router.get('/create', function(req, res, next) {
