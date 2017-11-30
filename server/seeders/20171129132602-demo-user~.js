116a6f7c81e3d0257f98b31277359176c9f102fd
Balthazar Caillaud <balthacaillaud@hotmail.fr>
2017-11-30 11:19:27 +0100

    Ajout du dossier seeders permettant de générer des fausse données en utilisant le module faker

---
+++ server/seeders/20171129132602-demo-user~.js
@@ -0,0 +1,24 @@
+'use strict';
+var faker = require('faker');
+
+module.exports = {
+  up : function (queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('users', [{
+      firstname: faker.name.firstName(),
+      name: faker.name.lastName(),
+      email: faker.internet.email(),
+      password: faker.lorem.sentence(),
+      dateofbirth: faker.date.recent(),
+      address: faker.lorem.sentence(),
+      zipcode: faker.lorem.sentence(),
+      city: faker.lorem.sentence(),
+      phone: faker.lorem.sentence(),
+      CreatedAt: faker.date.recent(),
+      UpdatedAt: faker.date.recent()
+    }], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+ return queryInterface.bulkDelete('Users', null, {});
+    }
+  };
