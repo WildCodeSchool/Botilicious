ab08d0bba9059ea643670d96c5eaa4003a808cc8
jeremy d <jeremy.david94@gmail.com>
2017-12-06 12:12:33 +0100

    resolution conflit lors du pull de la dev

--- controllers/Indexes.js
+++ controllers/Indexes.js
@@ -40,7 +40,7 @@ var Indexes = {
                 console.log(error);
                 res.render('index', {error: error});
             }else{
-                res.redirect('/confirminscription');
+                res.redirect('/confirmationinscription');
             }
     },
 
@@ -70,67 +70,8 @@ var Indexes = {
           });       
 
     },
-
-    confirmationinscription: function (req, res, next) {
-            res.render('index/confirminscription');
-    },
-
-    connexionGet: function (req, res, next) {
-        res.render('admin/configchat');
-    },
-
-
-    deconnexion: function (req, res, next) {
-        res.render('index/deconnexion');
-    },
-    
-
-    motdepasseoublie: function (req, res, next) {
-        res.render('index/oubli');
-    },
-
-    connexionPost: function (req, res, next) {
-        let error=[];
-        console.log(validator.isEmpty(req.body.email));
-        if(validator.isEmpty(req.body.email) ){
-            error[0] = "Merci de renseigner votre identifiant";
-            console.log(error.length);
-        }
-        if(validator.isEmpty(req.body.motdepasse) ){
-            error[1] = "Merci de renseigner votre mot de passe";
-            console.log(error.length);
-        }
-        if(error.length>0){
-            console.log(error);
-            res.render('login', {error: error});
-        }else{
-            res.redirect('/connexion');
-        }
-    },
-        /* console.log('login en cours');
-        console.log(req.body);
-        let login = req.body.email;
-        let pass = req.body.motdepasse;
-        console.log(login, pass); */
-    
-    // requête sequelize sur la table Users
-        // connection.query('SELECT * FROM users WHERE email = ? AND password = ? ;',[login, pass],function (error, results, fields) {
-        //   if (error) throw error;
-        //   if (results.length === 0) {
-        //     console.log('error login');
-        //     res.redirect('/connexion');
-        //   } else {
-        // req.session.connected = true;
-    
-    // inscrire l'id de l'utilisateur dans req.session
-        // req.session.user = results[0].id;
-        //console.log(req.session);
-        //res.redirect('/main/');
-    //},
-
-
-      
-
 };
+
+
 
 module.exports = Indexes; 
--- controllers/Logins.js
+++ controllers/Logins.js
@@ -10,12 +10,29 @@ var Logins = {
 
 // route POST '/connexion' -- Prise en compte du login
     connexionPost: function (req, res, next) {
-        console.log('login en cours');
+        let error=[];
+        console.log(validator.isEmpty(req.body.email));
+        if(validator.isEmpty(req.body.email) ){
+            error[0] = "Merci de renseigner votre identifiant";
+            console.log(error.length);
+        }
+        if(validator.isEmpty(req.body.motdepasse) ){
+            error[1] = "Merci de renseigner votre mot de passe";
+            console.log(error.length);
+        }
+        if(error.length>0){
+            console.log(error);
+            res.render('index/login', {error: error});
+        }else{
+            res.redirect('/configchat');
+        }
+    },
+        /* console.log('login en cours');
         console.log(req.body);
         let login = req.body.email;
         let pass = req.body.motdepasse;
-        console.log(login, pass);
-    
+        console.log(login, pass); */
+
     // requête sequelize sur la table Users
         // connection.query('SELECT * FROM users WHERE email = ? AND password = ? ;',[login, pass],function (error, results, fields) {
         //   if (error) throw error;
@@ -23,15 +40,18 @@ var Logins = {
         //     console.log('error login');
         //     res.redirect('/connexion');
         //   } else {
-        req.session.connected = true;
-    
+        // req.session.connected = true;
+
     // inscrire l'id de l'utilisateur dans req.session
         // req.session.user = results[0].id;
-        console.log(req.session);
-        res.redirect('/admin');
-    },
+        //console.log(req.session);
+        //res.redirect('/main/');
+    //},
 
-     
-};
 
-module.exports = Logins; 
+    
+
+}; 
+
+
+module.exports = Logins;
