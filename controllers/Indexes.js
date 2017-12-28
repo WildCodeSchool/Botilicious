b3b0b3d3cb175de3172521ebefd47639efd30505
Fabrice <fabwild@dejeammes.com>
2017-12-28 15:05:03 +0100

    inscription ok

--- controllers/Indexes.js
+++ controllers/Indexes.js
@@ -1,4 +1,6 @@
 const nodemailer = require('nodemailer');
+const validator = require('validator');
+const models = require('../models');
 
 const Indexes = {
 
@@ -14,7 +16,62 @@ const Indexes = {
   },
 
   indexPost(req, res) {
-    res.send('post index ok');
+    console.log(Object.entries(req.body));
+    const data = Object.entries(req.body);
+    // console.log(data);
+    // initalisation du tableau qui listera les erreurs
+    const error = [];
+    data.map((element, index) => {
+      console.log(element, index);
+      if (validator.isEmpty(element[1])) {
+        error[index + 1] = `Merci de renseigner votre ${element[0]}`;
+      }
+      return error;
+    });
+    // console.log('error: ', error);
+    // / y'a-t-il des erreurs ?
+    if (error.length > 0) {
+      // pour remplir toutes les cases jusqu'à la 9ème
+      error[10] = 'fin des erreurs';
+      console.log('error: ', error);
+      console.log('req.body: ', req.body);
+      res.render('index/index', { body: req.body, error });
+    } else {
+      // construire l'objet à donner à sequelize
+      const myparams = {
+        firstname: req.body.firstname,
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        dateofbirth: req.body.dateofbirth,
+        address: req.body.address,
+        zipcode: req.body.zipcode,
+        city: req.body.city,
+        phone: req.body.phone,
+      };
+      console.log('myparams: ', myparams);
+
+      // vérifier l'existence ou écrire l'objet
+      models.User
+        .findOrCreate({ where: myparams })
+        .then((results) => {
+          // l'utilisateur vérifié dans la bdd
+          console.log('results[0].dataValues: ', results[0].dataValues);
+          // le boolén qui indique si l'utilisateur a été crée ou pas
+          console.log('results[1]: ', results[1]);
+          if (!results[1]) {
+            error[0] = 'Email déjà présent';
+            console.log('error db, redirection: ', error);
+            res.render('index/index', { body: req.body, error });
+          } else {
+            req.session.connected = true;
+            // inscrire l'id de l'utilisateur dans req.session
+            req.session.userId = results.id;
+            console.log('req.session: ', req.session);
+            res.redirect('/admin/chatbotEdit');
+          }
+        });
+    }
   },
 
   // route GET -- envoie de mail pour confirmer son inscription
