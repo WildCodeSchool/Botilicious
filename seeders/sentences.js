c73f5bb14502148dde6f4ac6321598c8904c9b91
jeremy d <jeremy.david94@gmail.com>
2018-01-12 12:27:29 +0100

    ajout du script pour la demonstration

--- seeders/sentences.js
+++ seeders/sentences.js
@@ -3,30 +3,7 @@ const uuidv4 = require('uuid/v4');
 
 module.exports = {
   up(queryInterface, Sequelize) {
-    return queryInterface.bulkInsert('Sentences', [{
-      uuid: uuidv4(),
-      text: 'Quel temps fait-il demain à Paris ?',
-      type: 'question',
-      next: 2,
-      CreatedAt: faker.date.recent(),
-      UpdatedAt: faker.date.recent(),
-    },
-    {
-      uuid: uuidv4(),
-      text: 'Quel temps fait-il demain à San Fransisco ?',
-      type: 'question',
-      next: 1,
-      CreatedAt: faker.date.recent(),
-      UpdatedAt: faker.date.recent(),
-    },
-    {
-      uuid: uuidv4(),
-      text: 'Bonjour Fransisco',
-      type: 'answer',
-      next: 1,
-      CreatedAt: faker.date.recent(),
-      UpdatedAt: faker.date.recent(),
-    }], {});
+    return queryInterface.bulkInsert('Sentences', [], {});
   },
 
   down: (queryInterface, Sequelize) => queryInterface.bulkDelete('Sentences', null, {}),
