7f48a5a2c72084f9aca585793925c8f76b773130
jeremy d <jeremy.david94@gmail.com>
2018-01-26 12:02:55 +0100

    positionnement fenêtre de chat a gauche en css en cours

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -28,3 +28,29 @@
 //         $("#top_nav").removeClass("fixed");
 //     }
 // });
+
+
+// $(document).ready(function(){
+//     var eTop = $("#pageWithHeaderAndFooter  ).offset().top; 
+//     // offset() = get the current position of #alPage 
+//     // returns an object containing the properties 'top' and 'left' 
+//     $(document).scrollTop(eTop); 
+//     // set the current position of the scroll bar for 'etop'
+//     var eHeight = $('#pageWithHeaderAndFooter').height();
+//     // get the current height of '#allPage'
+//     var eBottom = eTop + eHeight - $(window).height();
+//     $(document).on("scroll", function(e){
+//         var windowScrollTop = $(window).scrollTop();
+//         if(windowScrollTop < eTop){
+//             console.log("not allowed");
+//             $(document).scrollTop(eTop);
+//         }
+//         else if(windowScrollTop > eBottom){
+//             $(document).scrollTop(eBottom);
+//         }
+//         else{
+//             console.log("allowed");
+//         }
+//     });
+// });
+
